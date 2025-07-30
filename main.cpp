ba90bcbce470f890766a412861b345f831a5c353
Anubhav20747 <chaudharyanubhav7457@gmail.com>
2025-07-30 11:19:49 +0530

    main.cpp

--- main.cpp
+++ main.cpp
@@ -1,17 +1,23 @@
-#include <iostream>
-using namespace std;
+#include <stdio.h>
 
 int main() {
-    int rows;
-    cout << "Enter number of rows: ";
-    cin >> rows;
+    int number, reversed;
 
-    for(int i = 1; i <= rows; ++i) {
-        for(int j = 1; j <= i; ++j) {
-            cout << "* ";
-        }
-        cout << endl;
+    printf("Enter a 3-digit number: ");
+    scanf("%d", &number);
+
+    if (number < 100 || number > 999) {
+        printf("Invalid input! Please enter a 3-digit number.\n");
+        return 1;
     }
+
+    int hundreds = number / 100;
+    int tens = (number / 10) % 10;
+    int units = number % 10;
+
+    reversed = units * 100 + tens * 10 + hundreds;
+
+    printf("Reversed number: %d\n", reversed);
 
     return 0;
 }
